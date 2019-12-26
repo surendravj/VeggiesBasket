b22ce515283fba9decb14c46093a32924963eb96
surendra <vadaparthysurendra@gmail.com>
2019-12-26 15:01:14 +0530

    Intial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//external packages importing section
+const express=require('express');
+const bodyparser=require('body-parser');
+const app=express();
+const port=process.env.port||5000;
+const home=require('./routes/main')
+
+// middleware section
+app.use(express.static(__dirname + '/public'))
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json())
+app.set('view engine','ejs')
+
+
+//middleware for home-page
+app.use(home)
+
+
+//congif file for port listening
+app.listen(port,()=>{
+    console.log(`Server is running at port `)
+})
---
+++ routes/main.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const request = require('request')
+const router = express.Router()
+const url = 'https://veggiesbasket.com/shaw/shaw/api/emp/product.php'
+
+
+
+
+
+//router for home-page
+router.get('/', (req, res) => {
+    request.post(url, (error, response, body) => {
+        if (error) {
+            console.log(error)
+        }
+        var data = JSON.parse(body);
+        var offers = calculateOffers(data)
+        var renderData = {
+            veges: data,
+            discount: offers
+        }
+        res.render('index', renderData)
+    })
+})
+
+// function that return offers 
+calculateOffers = (arr) => {
+    let offers = []
+    for (let i = 0; i < arr.length; i++) {
+        let x = ((arr[i].reg1 - arr[i].pri1) / arr[i].reg1) * 100;
+        offer = 100 - x;
+        offers.push(100 - offer.toFixed(2))
+
+    }
+    return offers
+}
+
+module.exports = router
